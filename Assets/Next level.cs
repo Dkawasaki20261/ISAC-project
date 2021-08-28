b673a88035975702fe4c77be721cea5818b71736
Dkawasaki20261 <89683985+Dkawasaki20261@users.noreply.github.com>
2021-08-28 17:30:02 +0900

    init

---
+++ Assets/Next level.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour
+{
+public int LevelNum;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+	{
+		Application.Quit();
+	}
+
+    }
+    
+    void OnTriggerEnter(Collider other)
+    {
+	if(other.tag == "Player")
+        {
+		SceneManager.LoadScene(LevelNum);
+        }
+    }
+
+
+}
---
+++ Assets/Stylized Astronaut/Character/Player.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+		private Animator anim;
+		private CharacterController controller;
+
+		public float speed = 600.0f;
+		public float turnSpeed = 400.0f;
+		public float gravity = 20.0f;
+		public float jumpPower = 500.0f;
+
+		void Start () {
+			controller = GetComponent <CharacterController>();
+			anim = gameObject.GetComponentInChildren<Animator>();
+		}
+
+		void Update (){
+
+			float moveHorizontal = Input.GetAxis("Horizontal");
+			float moveVertical = Input.GetAxis("Vertical");
+			var moveDirection = new Vector3(moveHorizontal,0,moveVertical);
+			float moveHeight = 0;
+			if (Input.GetAxis("Vertical") != 0) {
+				anim.SetInteger ("AnimationPar", 1);
+			}  else {
+				anim.SetInteger ("AnimationPar", 0);
+			}
+
+			if (Input.GetKeyDown(KeyCode.Space)){
+				Debug.Log("jumpPressed");
+				moveHeight = Mathf.Sqrt(jumpPower* -3.0f *gravity);
+			}
+
+			if(controller.isGrounded){
+				Vector3 movement = new Vector3(moveHorizontal, moveHeight, moveVertical);
+				moveDirection = movement;
+			}
+
+			
+			
+//			transform.Rotate(0, turn * turnSpeed * Time.deltaTime, 0);
+			controller.Move(moveDirection * speed * Time.deltaTime);
+			moveDirection.y -= gravity * Time.deltaTime;
+		}
+}
